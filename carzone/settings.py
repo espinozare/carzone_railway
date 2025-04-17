d5f8b1733026bad8c726f00a3e182a2ab69965bf
espinozare <203875783+espinozare@users.noreply.github.com>
2025-04-17 15:39:16 +0100

    Completed Team Models

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'code2025',
+        'PASSWORD': '12AtmosR',
+        'HOST': 'localhost',
     }
 }
 
@@ -121,6 +124,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),        
 ]
+
+# Media files (User-uploaded files)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
